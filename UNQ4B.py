f999fdca172db8e930d2aeff27bf4fc28d8ef41b
Brijesh Kumar Mishra <53488477+Unio-IoT@users.noreply.github.com>
2022-09-13 16:32:42 +0530

    Add files via upload

---
+++ UNQ4B.py
@@ -0,0 +1,13 @@
+import subprocess
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
+
+# GPIO18 set up as input. It is pulled up to stop false signals
+GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+# wait for the pin to be sorted with GND and, if so, halt the system
+GPIO.wait_for_edge(18, GPIO.FALLING)
+subprocess.call(['shutdown -h now "System halted by power switch "'], shell=True)
+
+# clean up GPIO on normal exit
+GPIO.cleanup()  
